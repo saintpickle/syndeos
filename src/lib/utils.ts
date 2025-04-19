34ab7be2053c7495c4603e6821c5fa2c5ab041ae
saintpickle <24785684+saintpickle@users.noreply.github.com>
2025-04-19 19:14:15 -0400

    created format date util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,21 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function fdate(timestamp: string): string {
+  const date = new Date(timestamp);
+
+  // Format options
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true
+  };
+
+  return date.toLocaleString('en-US', options);
+}
+
