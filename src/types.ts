04f256612dae88525e35b24d99237af0454300bf
saintpickle <24785684+saintpickle@users.noreply.github.com>
2025-04-19 19:14:02 -0400

    added types file

---
+++ src/types.ts
@@ -0,0 +1,33 @@
+export type Server = {
+    id?: number;
+    name: string;
+    hostname: string;
+    ip_address: string;
+    port: number;
+    username: string;
+    ssh_key_id?: number;
+    notes?: string;
+    created_at: string;
+    updated_at: string;
+};
+
+export type Servers = Server[];
+
+export type SshKey = {
+    id?: number;
+    name: string;
+    path: string;
+    is_default: boolean;
+    created_at: string;
+    updated_at: string;
+}
+
+export type SshKeys = SshKey[];
+
+export type Setting = {
+    id?: number;
+    key: string;
+    value: string;
+};
+
+export type Settings = Setting[];
