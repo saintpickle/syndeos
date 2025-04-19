42f0be07f70f6fd41552d0793fc5b76b1a4800c8
saintpickle <24785684+saintpickle@users.noreply.github.com>
2025-04-19 01:28:23 -0400

    updated code to use controller specific prefix to pull in functions

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,29 +4,11 @@ mod controllers;
 
 use tauri::AppHandle;
 use database::connection as conn;
-use controllers::ssh_key::{
-    init_ssh_keys,
-    add_ssh_key,
-    delete_ssh_key,
-    get_ssh_key,
-    get_ssh_keys,
-    generate_ssh_key,
-    set_default_ssh_key
-};
 
-use controllers::server::{
-    add_server,
-    delete_server,
-    get_server,
-    get_servers,
-    update_server
-};
-
-use controllers::setting::{
-    init_default_settings,
-    get_settings,
-    get_setting,
-    update_setting
+use controllers::{
+    ssh_key,
+    server,
+    setting,
 };
 
 #[tauri::command]
@@ -35,11 +17,11 @@ fn init_app(app_handle: AppHandle) -> Result<String, String> {
 
     println!("Tauri SQLite Database Initialization Successful!");
 
-    init_default_settings(app_handle.clone())?;
+    setting::init_default_settings(app_handle.clone())?;
 
     println!("Tauri Settings Initialization Successful!");
 
-    init_ssh_keys(app_handle.clone())?;
+    ssh_key::init_ssh_keys(app_handle.clone())?;
 
     println!("Tauri SSH Keys Initialization Successful!");
 
@@ -53,22 +35,22 @@ pub fn run() {
         .invoke_handler(tauri::generate_handler![
             init_app,
 
-            add_server,
-            get_server,
-            update_server,
-            delete_server,
-            get_servers,
+            server::add_server,
+            server::get_server,
+            server::update_server,
+            server::delete_server,
+            server::get_servers,
 
-            add_ssh_key,
-            delete_ssh_key,
-            get_ssh_key,
-            get_ssh_keys,
-            set_default_ssh_key,
-            generate_ssh_key,
+            ssh_key::add_ssh_key,
+            ssh_key::delete_ssh_key,
+            ssh_key::get_ssh_key,
+            ssh_key::get_ssh_keys,
+            ssh_key::set_default_ssh_key,
+            ssh_key::generate_ssh_key,
 
-            get_setting,
-            get_settings,
-            update_setting,
+            setting::get_setting,
+            setting::get_settings,
+            setting::update_setting, 
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
