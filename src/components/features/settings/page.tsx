280f5772b113d171a072f1a7a137f7e02ca61b6b
casellner <114782256+casellner@users.noreply.github.com>
2025-04-23 16:51:37 -0400

    Add Advanced Settings

--- src/components/features/settings/page.tsx
+++ src/components/features/settings/page.tsx
@@ -243,7 +243,34 @@ export default function SettingsPage() {
                     <Input id="encryption-password" type="password" className="bg-white mt-2" />
                 </div>
             </TabsContent>
-            <TabsContent value="advanced">TODO 6</TabsContent>
+            <TabsContent value="advanced">
+                <div className="my-3">
+                    <Label id="log-level">Log Level</Label>
+                    <Select aria-labeledby="log-level">
+                        <SelectTrigger className="w-[180px] bg-white mt-2">
+                            <SelectValue placeholder="Select a level" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectItem value="debug">Debug</SelectItem>
+                            <SelectItem value="info">Info</SelectItem>
+                            <SelectItem value="warn">Warning</SelectItem>
+                            <SelectItem value="error">Error</SelectItem>
+                        </SelectContent>
+                    </Select>
+                </div>
+                <div className="my-3">
+                    <Label>Proxy Configuration</Label>
+                    Coming Soon!
+                </div>
+                <div className="my-3">
+                    <Label>Custom Terminal Configuration</Label>
+                    Coming Soon!
+                </div>
+                <div className="my-3">
+                    <Label>Custom Script Paths</Label>
+                    Coming Soon!
+                </div>
+            </TabsContent>
         </Tabs>
     );
 }
