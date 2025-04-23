49b938c218e0ab33eb959d08c71ed8fe18366fb6
casellner <114782256+casellner@users.noreply.github.com>
2025-04-23 16:40:22 -0400

    Add backup and restore settings

--- src/components/features/settings/page.tsx
+++ src/components/features/settings/page.tsx
@@ -208,7 +208,41 @@ export default function SettingsPage() {
                     </div>
                 </div>
             </TabsContent>
-            <TabsContent value="backup">TODO 5</TabsContent>
+            <TabsContent value="backup">
+                <div className="my-3">
+                    <Label htmlFor="database-backup-location">Database Backup Location</Label>
+                    <Input id="database-backup-location" className="bg-white mt-2" />
+                </div>
+                <div className="my-3">
+                    <Label id="auto-backup-frequency">Auto-Backup Frequency</Label>
+                    <Select aria-labeledby="auto-backup-frequency">
+                        <SelectTrigger className="w-[180px] bg-white mt-2">
+                            <SelectValue placeholder="Select a frequency" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectItem value="never">Never</SelectItem>
+                            <SelectItem value="1h">Every hour</SelectItem>
+                            <SelectItem value="6h">Every 6 hours</SelectItem>
+                            <SelectItem value="12h">Every 12 hours</SelectItem>
+                            <SelectItem value="1d">Daily</SelectItem>
+                            <SelectItem value="7d">Weekly</SelectItem>
+                        </SelectContent>
+                    </Select>
+                </div>
+                <div className="flex items-center space-x-2 my-3">
+                    <Checkbox id="encrypt-backups" className="bg-white" />
+                    <label
+                        htmlFor="encrypt-backups"
+                        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                    >
+                        Encrypt Backups
+                    </label>
+                </div>
+                <div className="my-3">
+                    <Label htmlFor="encryption-password">Encryption Password</Label>
+                    <Input id="encryption-password" type="password" className="bg-white mt-2" />
+                </div>
+            </TabsContent>
             <TabsContent value="advanced">TODO 6</TabsContent>
         </Tabs>
     );
