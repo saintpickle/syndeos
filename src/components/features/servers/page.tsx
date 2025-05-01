a746d5fcb07384d41084592fbe253935855f6854
casellner <114782256+casellner@users.noreply.github.com>
2025-05-01 17:17:56 -0400

    Fix some backgrounds not changing for dark mode

--- src/components/features/servers/page.tsx
+++ src/components/features/servers/page.tsx
@@ -1,19 +1,19 @@
 export default function ServersPage() {
     return (
         <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="aspect-video rounded-xl bg-white text-3xl flex justify-center items-center">
+            <div className="aspect-video rounded-xl bg-white dark:bg-gray-900 text-3xl flex justify-center items-center">
                 <p>Server 1</p>
             </div>
-            <div className="aspect-video rounded-xl bg-white text-3xl flex justify-center items-center">
+            <div className="aspect-video rounded-xl bg-white dark:bg-gray-900 text-3xl flex justify-center items-center">
                 <p>Server 2</p>
             </div>
-            <div className="aspect-video rounded-xl bg-white text-3xl flex justify-center items-center">
+            <div className="aspect-video rounded-xl bg-white dark:bg-gray-900 text-3xl flex justify-center items-center">
                 <p>Server 3</p>
             </div>
-            <div className="aspect-video rounded-xl bg-white text-3xl flex justify-center items-center">
+            <div className="aspect-video rounded-xl bg-white dark:bg-gray-900 text-3xl flex justify-center items-center">
                 <p>Server 4</p>
             </div>
-            <div className="aspect-video rounded-xl bg-white text-3xl flex justify-center items-center">
+            <div className="aspect-video rounded-xl bg-white dark:bg-gray-900 text-3xl flex justify-center items-center">
                 <p>Server 5</p>
             </div>
         </div>
